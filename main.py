62204b20109300fb6da0d6becaf2bffedf690590
Steve Gossip <stephengossip@gmail.com>
2022-01-29 21:28:21 +0000

    installed selenium and added my initial constants for the project.

---
+++ main.py
@@ -0,0 +1,10 @@
+import selenium
+import os
+
+
+
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+CHROME_DRIVER_PATH = '/Users/admin/Development/chromedriver'
+TWITTER_EMAIL = os.environ.get('TWITTER_EMAIL')
+TWITTER_PASSWORD = os.environ.get('TWITTER_PASSWORD')
