bb4f22563cbffb5a539e47f5b0bb196699335a36
Steve Gossip <stephengossip@gmail.com>
2022-01-31 20:14:51 +0000

    successfully able to check my internet speed and then dependant on the result the bot will login to twitter, write a post and tweet it with the realtime speeds and promised speeds.

--- main.py
+++ main.py
@@ -50,40 +50,47 @@ class InternetSpeedBot:
         self.up = float(self.driver.find_element(By.CLASS_NAME, 'upload-speed').text)
         self.down = float(self.driver.find_element(By.CLASS_NAME, 'download-speed').text)
 
-    def tweet_at_provider(self, url, provider):
-        self.driver.get(url)
-        self.driver.maximize_window()
-        time.sleep(5)
+    def tweet_at_provider(self, url, provider, promised_up, promised_down):
+        self.get_internet_speed()
+        if self.up < promised_up or self.down < promised_down:
+            self.driver.get(url)
+            self.driver.maximize_window()
+            time.sleep(5)
 
-        # ENTER EMAIL ADDRESS AND NEXT
-        email_input = self.driver.find_element(By.NAME, 'text')
-        email_input.send_keys(TWITTER_EMAIL)
-        next_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/'
-                                                         'div/div/div[2]/div[2]/div[1]/div/div[6]')
-        next_button.click()
-        # POSSIBLE 'UNUSUAL LOGIN ACTIVITY' PAGE
-        try:
-            time.sleep(3)
-            username_input = self.driver.find_element(By.NAME, 'text')
-            username_input.send_keys(TWITTER_USERNAME)
-            next_button_2 = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]'
-                                                               '/div/div/div[2]/div[2]/div[2]/div/div')
-            next_button_2.click()
-        except NoSuchElementException:
-            print("No unusual activiy page")
-        # ENTER PASSWORD AND LOGIN
-        time.sleep(5)
-        password_input = self.driver.find_element(By.NAME, 'password')
-        password_input.send_keys(TWITTER_PASSWORD)
-        login_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/'
-                                                          'div/div/div[2]/div[2]/div[2]/div/div/div')
-        login_button.click()
+            # ENTER EMAIL ADDRESS AND NEXT
+            email_input = self.driver.find_element(By.NAME, 'text')
+            email_input.send_keys(TWITTER_EMAIL)
+            next_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/'
+                                                             'div/div/div[2]/div[2]/div[1]/div/div[6]')
+            next_button.click()
+            # POSSIBLE 'UNUSUAL LOGIN ACTIVITY' PAGE
+            try:
+                time.sleep(3)
+                username_input = self.driver.find_element(By.NAME, 'text')
+                username_input.send_keys(TWITTER_USERNAME)
+                next_button_2 = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]'
+                                                                   '/div/div/div[2]/div[2]/div[2]/div/div')
+                next_button_2.click()
+            except NoSuchElementException:
+                print("No unusual activiy page")
+            # ENTER PASSWORD AND LOGIN
+            time.sleep(5)
+            password_input = self.driver.find_element(By.NAME, 'password')
+            password_input.send_keys(TWITTER_PASSWORD)
+            login_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/'
+                                                              'div/div/div[2]/div[2]/div[2]/div/div/div')
+            login_button.click()
 
-        tweet = f"Hey {provider}, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}" \
-                f"down/{PROMISED_UP}up?"
-        time.sleep(5)
-        whats_happening_input = self.driver.find_element(By.CLASS_NAME, 'public-DraftStyleDefault-block')
-        whats_happening_input.send_keys(tweet)
+            tweet = f"Hey {provider}, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}" \
+                    f"down/{PROMISED_UP}up?"
+            time.sleep(5)
+
+            # WRITE AND SEND TWEET
+            whats_happening_input = self.driver.find_element(By.CLASS_NAME, 'public-DraftStyleDefault-block')
+            whats_happening_input.send_keys(tweet)
+            send_tweet = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div'
+                                                            '/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]')
+            send_tweet.click()
 
 
 
@@ -92,7 +99,7 @@ class InternetSpeedBot:
 speed_test = InternetSpeedBot(CHROME_DRIVER_PATH)
 # print("Down before: ", speed_test.get_download_speed())
 # print("Up before: ", speed_test.get_upload_speed())
-# speed_test.get_internet_speed(SPEED_TEST_URL)
+# speed_test.get_internet_speed()
 # print("Down after: ", speed_test.get_download_speed())
 # print("Up after: ", speed_test.get_upload_speed())
-speed_test.tweet_at_provider(TWITTER_URL, PROVIDER)
+speed_test.tweet_at_provider(TWITTER_URL, PROVIDER, PROMISED_UP, PROMISED_DOWN)
